2c34524c1a1d1a6acb5b75917b27fe35f3ff944b
nursh <nsheikhmohammed@yahoo.com>
2019-06-11 17:04:08 -0400

    Added proxy for back-end server and confirmation for deleting Project

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,16 +1,10 @@
-import axios from 'axios';
-import {
-  GET_ERRORS,
-  GET_PROJECTS,
-  GET_PROJECT,
-  DELETE_PROJECT
-} from './types';
-
+import axios from "axios";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post('http://localhost:8080/api/projects', project);
-    history.push('/dashboard');
+    const res = await axios.post("/api/projects", project);
+    history.push("/dashboard");
     dispatch({
       type: GET_ERRORS,
       payload: {}
@@ -21,32 +15,34 @@ export const createProject = (project, history) => async dispatch => {
       payload: error.response.data
     });
   }
-} 
+};
 
 export const getProjects = () => async dispatch => {
-  const res = await axios.get('http://localhost:8080/api/projects');
+  const res = await axios.get("/api/projects");
   dispatch({
     type: GET_PROJECTS,
     payload: res.data
   });
-}
+};
 
 export const getProject = (id, history) => async dispatch => {
   try {
-    const res = await axios.get(`http://localhost:8080/api/projects/${id}`);
+    const res = await axios.get(`/api/projects/${id}`);
     dispatch({
       type: GET_PROJECT,
       payload: res.data
     });
   } catch (error) {
-    history.push('/dashboard');
+    history.push("/dashboard");
   }
-}
+};
 
-export const deleteProject = (id) => async dispatch => {
-  await axios.delete(`http://localhost:8080/api/projects/${id}`);
-  dispatch({
-    type: DELETE_PROJECT,
-    payload: id
-  })
-}
+export const deleteProject = id => async dispatch => {
+  if (window.confirm("Are you sure you want to delete this project")) {
+    await axios.delete(`/api/projects/${id}`);
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: id
+    });
+  }
+};
