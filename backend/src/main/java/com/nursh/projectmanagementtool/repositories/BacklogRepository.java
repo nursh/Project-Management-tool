5a0acf2efca69fd28d2c8060ff4f3862c4ebd5da
nursh <nsheikhmohammed@yahoo.com>
2019-06-12 01:02:46 -0400

    Added repositories for Backlog and Task classes

---
+++ backend/src/main/java/com/nursh/projectmanagementtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.nursh.projectmanagementtool.repositories;
+
+import com.nursh.projectmanagementtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
